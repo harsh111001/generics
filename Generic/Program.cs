c0a10515d68d1b252c88b8b46db123c2164a1855
harsh <harshjain112001@gmail.com>
2023-01-22 15:53:30 +0530

    print different kinds of array using generic method

---
+++ Generic/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello");
+            int[] intarray = { 1, 2, 3, 4, 5, };
+            print<int>(intarray);
+            char[] chararray = { 'a', 'b','c','d'};
+            print<char>(chararray);
+            double[] doublearray = { 1.1, 2.2, 3.3, 4.4 };
+            print<double>(doublearray);
+        }
+        public static void print<T>(T[] array)
+        {
+            foreach(var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
